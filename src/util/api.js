0af9dceefa17e2dd9919ab028533676d935225bf
Aman Kumar Jha <amankumar9631400@gmail.com>
2024-07-10 10:10:35 +0530

    api key done

--- src/util/api.js
+++ src/util/api.js
@@ -5,7 +5,7 @@ const BASE_URL = "https://youtube138.p.rapidapi.com";
 const options = {
   params: { hl: "en", gl: "US" },
   headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
+    "X-RapidAPI-Key": process.env.REACT_APP_YOUTUBE_API_KEY,
     "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
   },
 };
